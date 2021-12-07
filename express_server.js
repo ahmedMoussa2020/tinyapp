a51f508f04b5b03497ccfcddc407b904d4fe14a1
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2021-12-07 22:11:45 +0000

    server has been commited

--- express_server.js
+++ express_server.js
@@ -3,26 +3,55 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.set("view engine", "ejs");
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRaandomString() {
+  let len = 6;
+  return Math.randon().toString(20).substr(2, '${len > 6 ? (len = 6) : (len = 6)}');
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL.trim()].longURL};
   res.render("urls_show", templateVars);
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
